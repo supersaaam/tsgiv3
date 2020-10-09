5dea1e20ba6d3d205c423088d16a10424c7cc27a
supersaaam <samboygeron@gmail.com>
2020-10-09 22:09:25 +0800

    connection string updated

--- models/connection.php
+++ models/connection.php
@@ -2,10 +2,10 @@
 	/*ini_set("display_errors", 0);
 	error_reporting(0);*/
 	
-	$db		= "heroku__8b68aad9c22988c";
+	$db		= "heroku_8b68aad9c22988c";
 	$user		= "b88802b15e6949";
 	$pw		= "8d7ede0b";
-    	$server		= "us-cdbr-east-02.cleardb.com";
+    $server		= "us-cdbr-east-02.cleardb.com";
     
 	// Create connection
 	$con = new mysqli($server, $user, $pw, $db);
@@ -16,11 +16,11 @@
 	}
 ?>
 
-
-/*
-mysql://
-user: b88802b15e6949
-pass: 8d7ede0b
-hostname:us-cdbr-east-02.cleardb.com/
+ysql://
+b88802b15e6949
+:
+8d7ede0b
+@
+us-cdbr-east-02.cleardb.com
+/
 heroku_8b68aad9c22988c?reconnect=true
-*/
