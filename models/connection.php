2dcbdc61bb70776ce1952a660ace4d05cd9718d3
supersaaam <samboygeron@gmail.com>
2020-10-09 21:57:46 +0800

    commit new connection.php

--- models/connection.php
+++ models/connection.php
@@ -2,10 +2,10 @@
 	/*ini_set("display_errors", 0);
 	error_reporting(0);*/
 	
-	$db		= "tsgi";
-	$user		= "transcendo_admin";
-	$pw		= "transcendo_admin";
-    	$server		= "localhost";
+	$db		= "heroku__8b68aad9c22988c";
+	$user		= "b88802b15e6949";
+	$pw		= "8d7ede0b";
+    	$server		= "us-cdbr-east-02.cleardb.com";
     
 	// Create connection
 	$con = new mysqli($server, $user, $pw, $db);
@@ -15,3 +15,12 @@
 		die('Connection failed: ' . $con->connect_error);
 	}
 ?>
+
+
+/*
+mysql://
+user: b88802b15e6949
+pass: 8d7ede0b
+hostname:us-cdbr-east-02.cleardb.com/
+heroku_8b68aad9c22988c?reconnect=true
+*/
